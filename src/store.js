aaa7a1d47fef97b57e448870586da597095ccc1f
Sogand <sogandmohamadipour7@gmail.com>
2025-12-31 22:21:53 +0330

    still refactoring

--- src/store.js
+++ src/store.js
@@ -1,33 +1,6 @@
 import { combineReducers, createStore } from 'redux';
-
-
-
-const initialStateCustomer = {
-    fullName: '',
-    nationalID: '',
-    createdAT: '',
-};
-
-
-
-function customerReducer(state = initialStateCustomer, action) {
-    switch (action.type) {
-        case 'customer/createCustomer':
-            return {
-                ...state,
-                fullName: action.payload.fullName,
-                nationalID: action.payload.nationalID,
-                createdAT: action.payload.createdAT,
-            };
-        case 'customer/updateName':
-            return {
-                ...state,
-                fullName: action.payload,
-            };
-        default:
-            return state;
-    }
-};
+import accountReducer from './features/accounts/accountSlice';
+import customerReducer from './features/customers/customerSlice';
 
 const rootReducer = combineReducers({
     account: accountReducer,
@@ -35,18 +8,3 @@ const rootReducer = combineReducers({
 });
 
 const store = createStore(rootReducer);
-
-// ////////////////////////////////////////////
-function createCustomer(fullName, nationalID) {
-    return {
-        type: 'customer/createCustomer',
-        payload: { fullName, nationalID, createdAT: new Date().toISOString() },
-    }
-};
-
-function updateName(fullName) {
-    return {
-        type: 'customer/updateName',
-        payload: fullName,
-    }
-};
