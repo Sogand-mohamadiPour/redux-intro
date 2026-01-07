cb3ae03817b4a31dc48340f891ce8b0187bc08f6
Sogand <sogandmohamadipour7@gmail.com>
2026-01-07 21:55:31 +0330

    still refactoring

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -8,13 +8,30 @@ const initialState = {
 };
 
 const accountSlice = createSlice({
-    nsme: 'account',
+    name: 'account',
     initialState,
     reducers: {
         deposit(state, action) {
             state.balance += action.payload;
+        },
+        withdraw(state, action) {
+            state.balance -= action.payload;
+        },
+        requestLoan(state, action) {
+            if (state.loan > 0) return;
+            state.loan = action.payload.amount;
+            state.loanPurpose = action.payload.purpose;
+            state.balance += action.payload.amount;
+        },
+        payLoan(state, action) {
+            state.loan = 0;
+            state.loanPurpose = '';
+            state.balance -= state.loan;
         }
-
     }
-})
+});
+
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export default accountSlice.reducer;
 
